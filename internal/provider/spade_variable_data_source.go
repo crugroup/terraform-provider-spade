ba3c2ad2a4d9451f5f10233b6d6d1377561ac67a
Zakariah Nuccio <zak.nuccio@crugroup.com>
2025-08-01 00:27:59 +0100

    fix comment

--- internal/provider/spade_variable_data_source.go
+++ internal/provider/spade_variable_data_source.go
@@ -13,7 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Ensure provider defined types fully satisfy framework interfaces
+// Ensure provider defined types fully satisfy framework interfaces.
 var _ datasource.DataSource = &SpadeVariableDataSource{}
 
 func NewSpadeVariableDataSource() datasource.DataSource {
